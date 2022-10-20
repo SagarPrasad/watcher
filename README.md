1495ed8292cd0043a9e76be68da5aa44b5e80628
Sagar Prasad <sagar.prasad@ril.com>
2022-10-20 14:17:04 +0530

    Postgres integration with Flyway

---
+++ README.md
@@ -0,0 +1,14 @@
+```
+Pending:
+
+Conditional checks and separate client module
+Domain Builder (Done) and Validators 
+Kafka / Postgres Batch Updates - reduce commits
+
+JPA save is doing select / insert - avoid that.
+EndPoints Expose
+
+Integration for flyway - Done
+Postgres integration - Done
+
+```
--- src/main/java/com/sagar/audit/watcher/StreamListener.java
+++ src/main/java/com/sagar/audit/watcher/StreamListener.java
@@ -5,7 +5,6 @@ import io.cloudevents.CloudEvent;
 import io.cloudevents.core.data.PojoCloudEventData;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -15,16 +14,19 @@ import static io.cloudevents.core.CloudEventUtils.mapData;
 
 
 @Component
-public class StreamListner {
+public class StreamListener {
+  @Autowired
+  AuditMessageRepository auditMessageRepository;
   @Autowired
   ObjectMapper objectMapper;
 
-  @StreamListener(AuditMsgStream.INPUT)
+  @org.springframework.cloud.stream.annotation.StreamListener(AuditMsgStream.INPUT)
   public void handleAuditMsg(@Payload CloudEvent msg, @Headers Map<String, Object> headers) {
     System.out.println("msg recived " + msg);
     PojoCloudEventData<AuditMessage> cloudEventData = mapData(msg, PojoCloudEventDataMapper.from(objectMapper, AuditMessage.class));
     System.out.println("msg data name " + cloudEventData.getValue().getName());
-        //headers.get(KafkaHeaders.RECEIVED_PARTITION_ID), headers.get(KafkaHeaders.OFFSET));
+    auditMessageRepository.save(cloudEventData.getValue());
+    //headers.get(KafkaHeaders.RECEIVED_PARTITION_ID), headers.get(KafkaHeaders.OFFSET));
   }
 
 }
